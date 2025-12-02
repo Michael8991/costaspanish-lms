9d7632180e3ef323d4f9e041ef877f1c9f96646f
Michael Esequiel Rodríguez Iranzo <michael2002982@gmail.com>
2025-12-02 20:49:24 +0100

    Fixes: error de uso de any en rutas de prueba

--- app/api/seed-admin/route.ts
+++ app/api/seed-admin/route.ts
@@ -3,7 +3,17 @@ import User from "@/models/User";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
+
+
 export async function POST() {
+
+    if (process.env.NODE_ENV !== "development") {
+        return NextResponse.json(
+            { ok: false, message: "Esta ruta solo está disponible en modo desarrollo" },
+            { status: 403 } // 403 Forbidden
+        );
+    }
+
     try {
         await dbConnect();
 
@@ -15,7 +25,11 @@ export async function POST() {
                 message: "Admin already exists",
             });
         }
-        const password = "Admin1234!";
+
+        const password = process.env.MONGO_PSW_ADMIN_TEST;
+        if (!password) {
+             throw new Error("❌ FALTA LA VARIABLE DE ENTORNO: MONGO_PSW_ADMIN_TEST");
+        }
         const passwordHash = await bcrypt.hash(password, 10);
 
         const admin = await User.create({
@@ -38,8 +52,13 @@ export async function POST() {
                 password,
             }
         });
-    } catch (error: any) {
-        console.error("Error sedding admin:", error);
-        return NextResponse.json({ ok: false, error: error.message }, {status: 500})
+    } catch (error: unknown) {
+        console.error("Error seeding admin:", error);
+        
+        if (error instanceof Error) {
+             return NextResponse.json({ ok: false, error: error.message }, { status: 500 });
+        }
+
+        return NextResponse.json({ ok: false, error: "Error desconocido" }, { status: 500 });
     }
 }
